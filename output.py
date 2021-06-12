596b8d0505b1116c82f96a269a22042329f3d9ce
Henryzhaoheran <zhaoheran@126.com>
2021-06-12 13:09:48 +0800

    init output.py

---
+++ output.py
@@ -0,0 +1,19 @@
+# Test output
+box = 'too hot'
+temp = 30 # unit celsius
+book_price = 55.50 # unit RMB
+
+
+# format output
+'''
+    ("string$s"$variable)
+    %d integer
+    %f float
+'''
+print("weather: %s"%box)
+print("temperature is %sC"%temp)
+print("the book price is: %fCNY"%book_price)
+print("the book price is: %10.2fCNY"%book_price)
+print("the book price is: %10.4fCNY"%book_price)
+
+# 3.multiple values
