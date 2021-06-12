29f6ac054bbabde8a7221ff9ae22b2b3f7002849
Henryzhaoheran <zhaoheran@126.com>
2021-06-12 14:06:53 +0800

    chooce

---
+++ chooce.py
@@ -0,0 +1,15 @@
+import random # import tools
+
+# single choice
+
+# 1. generate random number
+# hour = 7 
+hour = random.randint(1, 24)
+print(hour)
+
+# 2. bool 
+if hour == 7:
+    print("get up")
+
+
+print("work")
