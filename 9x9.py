2f3d26b076f5c6171657919fa35e24f4adc22d93
Henryzhaoheran <zhaoheran@126.com>
2021-06-16 10:25:07 +0800

    9x92ndVersion

--- 9x9.py
+++ 9x9.py
@@ -1,2 +1,25 @@
 # Display 9x9 calculation
 
+'''
+#version 1
+for a in range(1,10):
+    for b in range(1,10):
+        if a >= b:
+            print("%i x %i = %i "%(b, a, a*b), end="\t")
+    print("")
+'''
+'''
+# improve. 
+# variable name changes to row and column
+
+'''
+row = 1
+while row <= 9:
+    column = 1
+    while row >= column: #and column <= 9:
+        print("%i x %i = %i"%(column, row, column*row), end="\t")
+        column = column + 1
+    print("")
+    row = row + 1
+   
+
