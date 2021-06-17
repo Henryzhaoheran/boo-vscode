bb8f231cda74ef151719c48a055db707e95e0572
Henryzhaoheran <zhaoheran@126.com>
2021-06-17 10:11:04 +0800

    init CalcWords

---
+++ calcWords.py
@@ -0,0 +1,11 @@
+'''
+user input a sentence and we calculate how many words are inputed
+
+'''
+
+s = input("Please input the sentence: ")
+print(s)
+ll = s.split(" ")
+print("There are total %i words"%len(ll))
+
+
