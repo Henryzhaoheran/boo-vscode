08387677729be3e6ffcf6d4ce5a6843dcba45b4e
Henryzhaoheran <zhaoheran@126.com>
2021-06-22 09:05:46 +0800

    Init_calulateWords

--- calcWords.py
+++ calcWords.py
@@ -3,9 +3,31 @@ user input a sentence and we calculate how many words are inputed
 
 '''
 
+'''
+#check if key in 
+
+car = {
+"brand": "Ford",
+"model": "Mustang",
+"year": 1964
+}
+x = car.keys()
+print(x)
+
+print("brand" in car)
+'''
+
 s = input("Please input the sentence: ")
 print(s)
 ll = s.split(" ")
 print("There are total %i words"%len(ll))
 
 
+mydict = dict()
+i = 0  # use i get all values in ll list.
+j = 1 # use j calculate word appear times.
+while i < len(ll):
+    if (ll[i] in mydict):
+        mydict[ll[i]]= mydict[ll[i]] + 1
+    else:
+        
