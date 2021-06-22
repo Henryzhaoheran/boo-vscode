a83e45218a9a0091698f17e84a4d8daa7ad6ac61
Henryzhaoheran <zhaoheran@126.com>
2021-06-22 09:33:52 +0800

    calcMyVersion

--- calcWords.py
+++ calcWords.py
@@ -23,11 +23,16 @@ ll = s.split(" ")
 print("There are total %i words"%len(ll))
 
 
+
 mydict = dict()
 i = 0  # use i get all values in ll list.
 j = 1 # use j calculate word appear times.
 while i < len(ll):
     if (ll[i] in mydict):
         mydict[ll[i]]= mydict[ll[i]] + 1
+        i=i+1
     else:
-        
+        mydict[ll[i]] = 1
+        i=i+1
+
+print(mydict)
---
+++ testdict.py
@@ -0,0 +1,7 @@
+testdict = dict()
+testdict = {'name1':'bob', 'name2':'jack', 'name3':'tom'}
+testdict['name4']='Will'
+testdict['name1']='Briget'
+print(testdict)
+
+print(type(testdict))
