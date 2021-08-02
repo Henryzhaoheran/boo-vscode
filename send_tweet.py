7e827a5038262a04fc8364d17a3324f05e9fe797
Henryzhaoheran <zhaoheran@126.com>
2021-08-02 20:34:46 +0800

    testdict.py

---
+++ send_tweet.py
@@ -0,0 +1,1 @@
+def send_tweet_with_image
--- testdict.py
+++ testdict.py
@@ -1,4 +1,30 @@
+# test Id
+import copy
+a = ['a', [1,2]] #list in list
+b = a.copy
+print(id(a))
+print(id(b))
+print(a)
+print(b)
+
+c = a
+d = copy.deepcopy(a)
+print(id(d[1]))
+print(id(a[1]))
+
+
+
+
 '''
+# Test list append
+user_list = []
+user_list.append("hello")
+print(user_list)
+'''
+
+
+'''
+# Test dict
 testdict = dict()
 testdict = {'name1':'bob', 'name2':'jack', 'name3':'tom'}
 testdict['name4']='Will'
@@ -8,7 +34,3 @@ print(testdict)
 print(type(testdict))
 '''
 
-
-user_list = []
-user_list.append("hello")
-print(user_list)
